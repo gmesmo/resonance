b52f4659e5f71cdd6093accf9ab83f8db6e0211b
gmesmo <guimachado94@gmail.com>
2023-05-18 11:43:30 -0300

    Changes to be committed: 	modified:   public/index.html 	modified:   src/App.module.css 	modified:   src/components/cover/mark.jsx 	modified:   src/components/cover/mark.module.css

--- src/components/cover/mark.jsx
+++ src/components/cover/mark.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import styles from './mark.module.css';
 
 const Cover = () => {
+    const LogoCreate = () => {
+        return(
+            <img src='../../mark.png' />
+        );
+        
+    }
+
     return ( <>
         <section id={styles.cover}>
             <div>
-                <img src='../../mark.png' />
+                <div className={styles.logo_}>
+                    <LogoCreate />
+                    <LogoCreate />
+                    <LogoCreate />
+                </div>
                 <h1>Resonance</h1>
             </div>
             
