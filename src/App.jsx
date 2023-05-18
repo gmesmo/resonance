f8085cf410d2447940f9d045eb752c9f6e4aa4fc
gmesmo <guimachado94@gmail.com>
2023-05-18 16:53:13 -0300

    Changes to be committed: 	modified:   src/App.jsx 	modified:   src/App.module.css 	new file:   src/components/chapters/Capitulo.jsx 	new file:   src/components/chapters/Capitulo.module.css 	deleted:    src/components/chapters/cap0.json 	new file:   src/components/chapters/list/cap0.json 	modified:   src/components/cover/mark.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 import Cover from './components/cover/mark';
 
+import prologo from './components/chapters//list/cap0.json';
+import Capitulo from './components/chapters/Capitulo';
+
 function App() {
   return (
     <div className="App">
       <Cover />
+      <Capitulo 
+        title={prologo.title}
+        data={prologo.date}
+        text={prologo.text}
+      />
     </div>
   );
 }
---
+++ src/components/chapters/Capitulo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styles from './Capitulo.module.css';
+
+const Capitulo = (props) => {
+    const Texto = props.text.map((text) =>
+        <p>{text}</p>
+    );
+
+    return (    <section>
+                    <h2>{props.title}</h2>
+                    <h5>{props.data}</h5>
+                    <div className={styles.chapt}>
+                        {Texto}
+                    </div>
+                </section> 
+            );
+}
+ 
+export default Capitulo;
