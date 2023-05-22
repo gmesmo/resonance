9602be2f15712e2fba810228303a1accd9849f41
gmesmo <guimachado94@gmail.com>
2023-05-22 13:50:47 -0300

    Changes to be committed: 	modified:   src/components/chapters/Capitulo.jsx 	modified:   src/components/chapters/list/chapters.json

--- src/components/chapters/Capitulo.jsx
+++ src/components/chapters/Capitulo.jsx
@@ -16,7 +16,7 @@ const Capitulo = (props) => {
 
     const chaptNumber = props.chaptNumber - 1;
 
-    return (    <section>
+    return (    <section key={props.chaptNumber}>
                     <h2>{chaptNumber > 0 ? `Capítulo ${chaptNumber} - ` : ``} {props.title}</h2>
                     <h5>Lançamento: {props.data}</h5>
                     <div className={styles.chapt}>
