390b5024675dd9a40d5be10cb4c679b622ceabca
gmesmo <guimachado94@gmail.com>
2023-05-23 17:46:43 -0300

    modified:   src/components/chapters/Capitulo.jsx

--- src/components/chapters/Capitulo.jsx
+++ src/components/chapters/Capitulo.jsx
@@ -5,6 +5,10 @@ import Typewriter from "typewriter-effect";
 
 import DateCheck from '../UI/DateCheck/DateCheck';
 
+import moment from 'moment';
+
+moment.locale('pt-br');
+
 const Capitulo = (props) => {
     const Texto = props.text.map((text) =>
         <Typewriter options={{delay: 0.001}} onInit={(typewriter) => {
@@ -14,10 +18,11 @@ const Capitulo = (props) => {
 
     const chaptNumber = props.chaptNumber - 1;
 
+    let data_cap = moment(props.data).format('L');
 
     return (    <section key={props.chaptNumber} className={<DateCheck data={props.data} /> ? `${styles.new}` : 'old'}>
                     <h2>{chaptNumber > 0 ? `Capítulo ${chaptNumber} - ` : ``} {props.title}</h2>
-                    <h5>Lançamento: {props.data}</h5>
+                    <h5>Lançamento: {data_cap}</h5>
                     <div className={styles.chapt}>
                         {Texto}
                     </div>
