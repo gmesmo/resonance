a56a4931858a3933f3da9a7f8cd070959f7600e7
gmesmo <guimachado94@gmail.com>
2023-05-24 14:40:48 -0300

    Changes to be committed: 	modified:   src/App.module.css 	modified:   src/components/UI/ChapterMiniature.jsx 	modified:   src/components/UI/ChapterMiniature.module.css 	modified:   src/components/UI/ChapterSelector.module.css 	modified:   src/components/chapters/Capitulo.jsx 	modified:   src/components/chapters/Capitulo.module.css

--- src/components/chapters/Capitulo.jsx
+++ src/components/chapters/Capitulo.jsx
@@ -7,7 +7,7 @@ import DateCheck from '../UI/DateCheck/DateCheck';
 
 import moment from 'moment';
 
-moment.locale('pt-br');
+moment.updateLocale('pt-br');
 
 const Capitulo = (props) => {
     const [skip, setSkip] = useState(false);
@@ -21,8 +21,6 @@ const Capitulo = (props) => {
     );
 
     if (skip) {
-        console.log(skip);
-
         Texto = props.text.map((text) =>
             <Typewriter key={'skipped'} onInit={(typewriter) => {
                 typewriter.pasteString(text).start();
@@ -36,17 +34,15 @@ const Capitulo = (props) => {
 
     const chaptNumber = props.chaptNumber - 1;
 
-    let data_cap = moment(props.data).format('L');
+    let data_cap = moment(props.data, 'YYYY-MM-DD').format('L');
 
     return (    <section key={props.chaptNumber} className={<DateCheck data={props.data} /> ? `${styles.new}` : 'old'}>
                     <h2>{chaptNumber > 0 ? `Capítulo ${chaptNumber} - ` : ``} {props.title}</h2>
                     <h5>Lançamento: {data_cap}</h5>
-                    <div className={styles.chapt}>
+                    <div className={`${styles.chapt} ${skip && styles.skipped}`}>
                         {Texto}
                     </div>
-                    {
-                        !skip && <button className={styles.skip} onClick={skipHandler}>SKIP</button>
-                    }
+                    {!skip && <button className={styles.skip} onClick={skipHandler}>SKIP</button>}
                 </section> 
             );
 }
