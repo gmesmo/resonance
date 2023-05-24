bf3b71c0559321953c6b9e8258dea0be56649da1
gmesmo <guimachado94@gmail.com>
2023-05-24 16:33:54 -0300

    Changes to be committed: 	modified:   src/components/UI/Button/Button.jsx 	new file:   src/components/UI/Button/Button.module.css 	modified:   src/components/chapters/Capitulo.jsx 	modified:   src/components/chapters/Capitulo.module.css

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import styles from "./Button.module.css";
+
 const Button = (props) => {
-  return <></>;
+  return (
+    <button className={styles.skip} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/chapters/Capitulo.jsx
+++ src/components/chapters/Capitulo.jsx
@@ -1,50 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import styles from './Capitulo.module.css';
+import styles from "./Capitulo.module.css";
 import Typewriter from "typewriter-effect";
 
-import DateCheck from '../UI/DateCheck/DateCheck';
+import DateCheck from "../UI/DateCheck/DateCheck";
 
-import moment from 'moment';
+import moment from "moment";
+import Button from "../UI/Button/Button";
 
-moment.updateLocale('pt-br');
+moment.updateLocale("pt-br");
 
 const Capitulo = (props) => {
-    const [skip, setSkip] = useState(false);
+  const [skip, setSkip] = useState(false);
 
-    
+  let Texto = props.text.map((text) => (
+    <Typewriter
+      key={"typping"}
+      options={{ delay: 0.001 }}
+      onInit={(typewriter) => {
+        typewriter.pauseFor(3000).typeString(text).start();
+      }}
+    />
+  ));
 
-    let Texto = props.text.map((text) =>
-        <Typewriter key={'typping'} options={{delay: 0.001}} onInit={(typewriter) => {
-            typewriter.pauseFor(3000).typeString(text).start();
-        }}/>
-    );
+  if (skip) {
+    Texto = props.text.map((text) => (
+      <Typewriter
+        key={"skipped"}
+        onInit={(typewriter) => {
+          typewriter.pasteString(text).start();
+        }}
+      />
+    ));
+  }
 
-    if (skip) {
-        Texto = props.text.map((text) =>
-            <Typewriter key={'skipped'} onInit={(typewriter) => {
-                typewriter.pasteString(text).start();
-            }}/>
-        );  
-    }
+  const skipHandler = () => {
+    setSkip(true);
+  };
 
-    const skipHandler = () => {
-        setSkip(true);
-    }
+  const chaptNumber = props.chaptNumber - 1;
 
-    const chaptNumber = props.chaptNumber - 1;
+  let data_cap = moment(props.data, "YYYY-MM-DD").format("L");
 
-    let data_cap = moment(props.data, 'YYYY-MM-DD').format('L');
+  return (
+    <section
+      key={props.chaptNumber}
+      className={<DateCheck data={props.data} /> ? `${styles.new}` : "old"}
+    >
+      <h2>
+        {chaptNumber > 0 ? `Capítulo ${chaptNumber} - ` : ``} {props.title}
+      </h2>
+      <h5>Lançamento: {data_cap}</h5>
+      <div className={`${styles.chapt} ${skip && styles.skipped}`}>{Texto}</div>
+      {!skip && (
+        <Button classes={"skip"} onClick={skipHandler}>
+          SKIP
+        </Button>
+      )}
+    </section>
+  );
+};
 
-    return (    <section key={props.chaptNumber} className={<DateCheck data={props.data} /> ? `${styles.new}` : 'old'}>
-                    <h2>{chaptNumber > 0 ? `Capítulo ${chaptNumber} - ` : ``} {props.title}</h2>
-                    <h5>Lançamento: {data_cap}</h5>
-                    <div className={`${styles.chapt} ${skip && styles.skipped}`}>
-                        {Texto}
-                    </div>
-                    {!skip && <button className={styles.skip} onClick={skipHandler}>SKIP</button>}
-                </section> 
-            );
-}
- 
 export default Capitulo;
