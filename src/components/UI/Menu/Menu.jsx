34c20a24850da2bd27176a3efdb8a31dd1427eb4
gmesmo <guimachado94@gmail.com>
2023-06-19 17:56:15 -0300

    Changes to be committed: 	modified:   src/components/UI/Menu/Menu.jsx  Changes not staged for commit: 	modified:   src/App.module.css 	modified:   src/components/UI/Button/Button.jsx 	modified:   src/components/UI/Button/Button.module.css 	modified:   src/components/UI/Menu/Menu.module.css 	modified:   src/components/UI/Menu/sections/Up.module.css

--- src/components/UI/Menu/Menu.jsx
+++ src/components/UI/Menu/Menu.jsx
@@ -6,11 +6,47 @@ import Up from "./sections/Up";
 import Button from "../Button/Button";
 
 const Menu = (props) => {
+  const svg = (
+    <svg
+      fill="var(--writing-color)"
+      className={styles.hamburger}
+      viewBox="0 0 100 100"
+      width="1.5rem"
+    >
+      <rect
+        className={`${styles.line} ${styles.top}`}
+        width="80"
+        height="10"
+        x="10"
+        y="25"
+        rx="5"
+      ></rect>
+      <rect
+        className={`${styles.line} ${styles.middle}`}
+        width="80"
+        height="10"
+        x="10"
+        y="45"
+        rx="5"
+      ></rect>
+      <rect
+        className={`${styles.line} ${styles.bottom}`}
+        width="80"
+        height="10"
+        x="10"
+        y="65"
+        rx="5"
+      ></rect>
+    </svg>
+  );
+
   return (
     <div id={styles.Menu}>
       <Up>
         <h1>Resonance</h1>
-        <Button classes={"default"}></Button>
+        <Button key_id={"menu"} classes={"default"}>
+          {svg}
+        </Button>
       </Up>
       <Down />
     </div>
