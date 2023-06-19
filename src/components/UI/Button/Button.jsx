bfedfa0ee0f1928ee64384415c1d3ce06f3c0baf
gmesmo <guimachado94@gmail.com>
2023-06-19 17:56:28 -0300

    Changes to be committed: 	modified:   src/App.module.css 	modified:   src/components/UI/Button/Button.jsx 	modified:   src/components/UI/Button/Button.module.css 	modified:   src/components/UI/Menu/Menu.module.css 	modified:   src/components/UI/Menu/sections/Up.module.css

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -3,6 +3,24 @@ import React from "react";
 import styles from "./Button.module.css";
 
 const Button = (props) => {
+  const key_id = props.key_id;
+
+  if (key_id === "menu") {
+    return (
+      <>
+        <input
+          key={key_id}
+          id={key_id}
+          className={styles[props.classes]}
+          onClick={props.onClick}
+          type={"checkbox"}
+          style={{ display: "none" }}
+        />
+        <label htmlFor={key_id}>{props.children}</label>
+      </>
+    );
+  }
+
   return (
     <button className={styles[props.classes]} onClick={props.onClick}>
       {props.children}
